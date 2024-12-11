7bce11a68bca6965413d740f859f04b5817728e1
jsonaguas <jaguas43@gmail.com>
2024-12-10 19:18:46 -0600

    added bootstrap navbar and router

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
 import UpdateProductForm from './components/UpdateProductForm';
 import OrderForm from './components/OrderForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route} from 'react-router-dom';
+import NaviBar from './components/NaviBar';
+
 import './App.css';
 
 class App extends Component {
@@ -52,23 +56,24 @@ class App extends Component {
     };
 
     addCustomer = (newCustomer) => {
-      this.setState(prevState => ({
-          customers: [...prevState.customers, newCustomer]
-      }));
-  };
-    addProduct = (newProduct) => {
-      this.setState(prevState => ({
-          products: [...prevState.products, newProduct]
-      }));
-  };
+        this.setState(prevState => ({
+            customers: [...prevState.customers, newCustomer]
+        }));
+    };
 
-  updateCustomerDetails = (updatedCustomer) => {
-      this.setState(prevState => ({
-          customers: prevState.customers.map(customer =>
-              customer.id === updatedCustomer.id ? updatedCustomer : customer
-          )
-      }));
-  };
+    addProduct = (newProduct) => {
+        this.setState(prevState => ({
+            products: [...prevState.products, newProduct]
+        }));
+    };
+
+    updateCustomerDetails = (updatedCustomer) => {
+        this.setState(prevState => ({
+            customers: prevState.customers.map(customer =>
+                customer.id === updatedCustomer.id ? updatedCustomer : customer
+            )
+        }));
+    };
 
     updateProductDetails = (updatedProduct) => {
         this.setState(prevState => ({
@@ -76,63 +81,79 @@ class App extends Component {
                 product.id === updatedProduct.id ? updatedProduct : product
             )
         }));
-    }
-
-
+    };
 
     render() {
-        const { selectedCustomerId, customers} = this.state;
-        const { selectedProductId, products } = this.state;
+        const { selectedCustomerId, customers, selectedProductId, products } = this.state;
         const selectedCustomer = customers.find(customer => customer.id === selectedCustomerId);
         const selectedProduct = products.find(product => product.id === selectedProductId);
+
         return (
-            <div className="app-container">
-                <h1>Our Customers</h1>
-                <CustomerForm onAddCustomer={this.addCustomer} />
-                <CustomerList 
-                    customers={customers} 
-                    onCustomerSelect={this.handleCustomerSelect} 
-                    updateCustomerList={this.updateCustomerList}
-                />
-                {selectedCustomer && (
-                    <>
-                        <CustomerDetails 
-                            customerId={selectedCustomerId} 
-                            customer={selectedCustomer}
-                            onCustomerDeleted={this.handleCustomerDeleted}
-                        />
-                        <UpdateCustomerForm 
-                            customerId={selectedCustomerId} 
-                            onUpdateCustomerDetails={this.updateCustomerDetails}
-                        
-                        />
-                    </>
-                )}
-                <h1>Our Products</h1>
-                <ProductForm onAddProduct={this.addProduct} />
-                <ProductList 
-                    products={this.state.products} 
-                    onProductSelect={this.handleProductSelect} 
-                    updateProductList={this.updateProductList}
-                />
-                {selectedProduct && (
-                    <>
-                        <ProductDetails 
-                            productId={selectedProductId} 
-                            product={selectedProduct}
-                            onProductDeleted={this.handleProductDeleted}
-                        />
-                        <UpdateProductForm 
-                            productId={selectedProductId} 
-                            onUpdateProductDetails={this.updateProductDetails}
-                        
-                        />
-                    </>
-                )}
-                <OrderForm 
-                onCustomerSelect={this.handleCustomerSelect}
-                onProductSelect={this.handleProductSelect} />
-            </div>
+                <div className="app-container">
+                    <NaviBar />
+                    <Routes>
+                        <Route path="/customers" element={
+                            <>
+                                <h1>Our Customers</h1>
+                                <CustomerForm onAddCustomer={this.addCustomer} />
+                                <CustomerList 
+                                    customers={customers} 
+                                    onCustomerSelect={this.handleCustomerSelect} 
+                                    updateCustomerList={this.updateCustomerList}
+                                />
+                                {selectedCustomer && (
+                                    <>
+                                        <CustomerDetails 
+                                            customerId={selectedCustomerId} 
+                                            customer={selectedCustomer}
+                                            onCustomerDeleted={this.handleCustomerDeleted}
+                                        />
+                                        <UpdateCustomerForm 
+                                            customerId={selectedCustomerId} 
+                                            onUpdateCustomerDetails={this.updateCustomerDetails}
+                                        />
+                                    </>
+                                )}
+                            </>
+                        } />
+                        <Route path="/products" element={
+                            <>
+                                <h1>Our Products</h1>
+                                <ProductForm onAddProduct={this.addProduct} />
+                                <ProductList 
+                                    products={products} 
+                                    onProductSelect={this.handleProductSelect} 
+                                    updateProductList={this.updateProductList}
+                                />
+                                {selectedProduct && (
+                                    <>
+                                        <ProductDetails 
+                                            productId={selectedProductId} 
+                                            product={selectedProduct}
+                                            onProductDeleted={this.handleProductDeleted}
+                                        />
+                                        <UpdateProductForm 
+                                            productId={selectedProductId} 
+                                            onUpdateProductDetails={this.updateProductDetails}
+                                        />
+                                    </>
+                                )}
+                            </>
+                        } />
+                        <Route path="/orders" element={
+                            <>
+                                <h1>Place an Order</h1>
+                                <OrderForm 
+                                    customers={customers} 
+                                    products={products} 
+                                    onCustomerSelect={this.handleCustomerSelect} 
+                                    onProductSelect={this.handleProductSelect}
+                                />
+                            </>
+                        } />
+                        <Route path="/" element={<h1>Welcome to Our Store</h1>} />
+                    </Routes>
+                </div>
         );
     }
 }
---
+++ src/components/NaviBar.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import {Navbar, Nav} from 'react-bootstrap';
+
+function NaviBar () {
+    return (
+        <Navbar expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to="/customers" activeclassname="active">
+              Customers
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/products" activeclassname="active">
+              Products
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/orders" activeclassname="active">
+              Orders
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+  
+  export default NaviBar;
